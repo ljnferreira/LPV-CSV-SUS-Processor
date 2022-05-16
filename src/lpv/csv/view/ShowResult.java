64ccec45c843d3d7883035a45441066ff3eed56a
Lucas Ferreira <ljnferreira@gmail.com>
2022-05-16 08:02:27 -0300

    refactor(ShowResult): removing unecessary main method

--- src/lpv/csv/view/ShowResult.java
+++ src/lpv/csv/view/ShowResult.java
@@ -20,20 +20,7 @@ public class ShowResult extends JDialog {
 	 */
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args, double Double) {
-		try {
-			ShowResult dialog = new ShowResult(Double);
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
+	
 	/**
 	 * Create the dialog.
 	 */
